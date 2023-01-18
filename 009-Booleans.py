bba895c0be24ac666e6158c9885d03c7ad04abb0
Zsolt Serbán <serban.zsolt@gmail.com>
2023-01-18 17:03:27 +0100

    add Boleans exercise

---
+++ 009-Booleans.py
@@ -0,0 +1,10 @@
+
+#? =============================================================================
+#* 009 - Booleans
+#? =============================================================================
+
+#* Booleans are logic operators, Can be True or False
+#* They are working with comparision operators usually
+
+print("Type of True: {}".format(type(True)))
+print("1>2: {}".format(1>2))
