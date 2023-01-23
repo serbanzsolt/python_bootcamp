44353cc0b66097ea89b62e4b73060778d0313ed1
Zsolt Serbán <serban.zsolt@gmail.com>
2023-01-23 14:38:19 +0100

    add comparison and logical operators exercise

---
+++ 011-Comparison_operators.py
@@ -0,0 +1,26 @@
+
+#? =============================================================================
+#* 011 - Comparison operators
+#? =============================================================================
+
+#* Equality
+
+print(2==2)
+print('hello' == 'bye')
+print('bye' == 'Bye')
+print(2.0 == 2)
+
+#* Inequality
+
+#Not-Equal
+print(3 != 3)
+print(4 != 5)
+
+#Lesser than
+print(2 < 4)
+
+#Greater than
+print(2 > 1)
+
+#Greater than or Equal
+print(2 >= 2)
