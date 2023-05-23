a362f514425d94f6c3a19256b5ec2f8afd1377a5
Zsolt Serbán <serban.zsolt@gmail.com>
2023-05-23 13:44:56 +0200

    add decorators advance exercises

---
+++ 038ADV2-Decorators.py
@@ -0,0 +1,16 @@
+import random
+
+def random_power():
+    def f(x):
+        return x**2
+    def g(x):
+        return x**3
+    def h(x):
+        return x**4
+    
+    functions = [f,g,h]
+    return random.choice(functions)
+
+for i in range(10):
+    p = random_power()
+    print(p(10))
